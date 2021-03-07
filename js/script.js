0177a35fe544e88bdc4bdf9792af388e33b20143
Artem <pushakartem@gmail.com>
2021-03-07 23:18:36 +0200

    added new features to web-site

---
+++ js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+
+        function hideTabContent () {
+            tabsContent.forEach(item => {
+                // item.style.display = 'none'; //or
+                item.classList.add('hide');
+                item.classList.remove('show', 'fade');
+            });
+
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+            });
+        }
+
+        function showTabContent (i = 0) { // i = 0 - означає що 0 це значення по замовчуванню, коли в аргумент ніц не передається "showTabContent()"
+            // tabsContent[i].style.display = 'block'; //or
+            tabsContent[i].classList.add('show', 'fade');
+            tabsContent[i].classList.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+        }
+
+        hideTabContent();
+        showTabContent();
+
+// the most important pert
+        tabsParent.addEventListener('click', (event) => {
+            const target = event.target;
+
+            if (target && target.classList.contains('tabheader__item')) {
+                tabs.forEach((item, i) => {
+                    if (target == item) {
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                })
+            }
+        });
+});
